96700f4d8134123c52b21380cd07c0bcc8e6a84c
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-22 20:23:45 +0700

    animal

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Animal1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfAnimal = 0;
+            Animal a = new Animal();
+            while (numOfAnimal >= 0)
+            {
+                
+                Console.WriteLine("Nhap ten dong vat: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Nhap tieng keu dong vat: ");
+                string sound = Console.ReadLine();
+                Console.WriteLine(a.idNum);
+                Console.WriteLine(name + " keu " + sound);
+                numOfAnimal++;
+            }
+        }
+    }
+}
